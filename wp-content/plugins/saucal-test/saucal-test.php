9b6a30b8c7338fc540cf9506b4c109b57ddfe2ba
David <davidlic@hotmail.com>
2018-05-15 14:26:50 -0300

    Plugin otimized with transients

--- wp-content/plugins/saucal-test/saucal-test.php
+++ wp-content/plugins/saucal-test/saucal-test.php
@@ -8,52 +8,20 @@
  * Text Domain: saucal_test
  */
 
-class saucal_test_widget extends WP_Widget {
+/* ----------------------------------------------
+   Content Setup
+------------------------------------------------*/
+function saucal_content(){
+		$transient_res = get_transient('saucal_test_remote_array');
 
+		$response = wp_remote_get( 'https://jsonplaceholder.typicode.com/posts?userId=1' );
+				     
+		$body = json_decode( $response['body'] );
 
-/* ------------------------------------------------
-	Widget Setup
------------------------------------------------- */
-
-	function saucal_test_widget() {
-		$widget_ops = array( 'classname' => 'saucal_test', 'description' => __('Saucal Test', 'saucal_test') );
-		$control_ops = array( 'width' => 300, 'height' => 350, 'id_base' => 'saucal_test' );
-		$this->WP_Widget( 'saucal_test', __('Saucal Test', 'saucal_test'), $widget_ops, $control_ops );
-	}
-
-	private function make_request( ) {
-	 
-	    $response = wp_remote_get( 'https://jsonplaceholder.typicode.com/posts?userId=1' );
-	    try {
-	 
-	        $json = json_decode( $response['body'] );
-	 
-	    } catch ( Exception $ex ) {
-	        $json = null;
-	    } // end try/catch
-	 
-	    return $json;
-	 
-	}
-/* ------------------------------------------------
-	Display Widget
------------------------------------------------- */
-	
-	function widget( $args, $instance ) {		
-		echo $before_widget;
-		 
-	?>
+		if ( ! $transient_res ){ ?>
 			<div class="saucal_test">
 				<div class="saucal_test_title">Saucal Test</div>
-				<div class="saucal_test_content">
-				    <?php
-
-				    $response = wp_remote_get( 'https://jsonplaceholder.typicode.com/posts?userId=1' );
-				     
-				    $body = json_decode( $response['body'] );
-				    
-				    ?>
-				     
+				<div class="saucal_test_content">				     
 				    <div class="saucal-test-list">
 				        <ul>
 				        <?php 
@@ -67,8 +35,56 @@ class saucal_test_widget extends WP_Widget {
 				        </ul> 
 				    </div>
 				</div>
+			</div>
+			<?php 
+				$transient_res = '<div class="saucal_test">
+				<div class="saucal_test_title">Saucal Test</div>
+				<div class="saucal_test_content">				     
+				    <div class="saucal-test-list">
+				        <ul>';
+				        if (!$body) 
+				                $transient_res .= '<li>Nothing to display!</li>';
+				        else
+				        foreach ( $body as $b ) { 
+				            $transient_res .= '<li><strong>'.$b->title.'</strong><br>
+				           '.$b->body.' </li>';
+				        }
+				        $transient_res .='</ul> 
+				    </div>
 				</div>
+			</div>';
+			?>
+			<?php set_transient('saucal_test_remote_array', $transient_res, DAY_IN_SECONDS );?>
+
 	<?php
+		}else{
+			echo get_transient('saucal_test_remote_array');
+		}
+}
+
+class saucal_test_widget extends WP_Widget {
+
+
+
+/* ------------------------------------------------
+	Widget Setup
+------------------------------------------------ */
+
+	function saucal_test_widget() {
+		$widget_ops = array( 'classname' => 'saucal_test', 'description' => __('Saucal Test', 'saucal_test') );
+		$control_ops = array( 'width' => 300, 'height' => 350, 'id_base' => 'saucal_test' );
+		$this->WP_Widget( 'saucal_test', __('Saucal Test', 'saucal_test'), $widget_ops, $control_ops );
+	}
+
+/* ------------------------------------------------
+	Display Widget
+------------------------------------------------ */
+	
+	function widget( $args, $instance ) {		
+		echo $before_widget;
+
+		saucal_content();
+		
 		echo $after_widget;
 	}	
 	
@@ -105,36 +121,5 @@ function saucal_test_tab_link_my_account( $items ) {
  
 add_filter( 'woocommerce_account_menu_items', 'saucal_test_tab_link_my_account' );
  
- 
- 
-function saucal_test_content() {
-?>
-<div class="saucal_test">
-				<div class="saucal_test_title">Saucal Test</div>
-				<div class="saucal_test_content">
-				    <?php
 
-				    $response = wp_remote_get( 'https://jsonplaceholder.typicode.com/posts?userId=1' );
-				     
-				    $body = json_decode( $response['body'] );
-				    
-				    ?>
-				     
-				    <div class="saucal-test-list">
-				        <ul>
-				        <?php 
-				        if (!$body) 
-				                echo '<li>Nothing to display!</li>';
-				        else
-				        foreach ( $body as $b ) { ?>
-				            <li><strong><?php echo $b->title; ?></strong><br>
-				           <?php echo $b->body;?> </li>
-				        <?php } ?>
-				        </ul> 
-				    </div>
-				</div>
-				</div>
-<?php 
-}
- 
-add_action( 'woocommerce_account_saucal-test_endpoint', 'saucal_test_content' );
+add_action( 'woocommerce_account_saucal-test_endpoint', 'saucal_content' );
